d9ddfdc05ab74c85b3155821f857c0bf8471426c
Ryan Au <ryau123@gmail.com>
2021-08-20 01:18:11 +0800

    update process feat ddcpca script

--- data/test_data_BU/process_feat_ddcpca.py
+++ data/test_data_BU/process_feat_ddcpca.py
@@ -1,22 +1,41 @@
 import numpy as np
 import pickle
+from pathlib import Path
+from sklearn.preprocessing import StandardScaler
 
 # refactor later for cmd "filename" and "scaler" input
 
-#filename
-filename = "Medium_dance_classical.feats_ddcpca.npy"
+# filename
+data_path = "./"
 
-#sklearn StandardScaler (n_features=85)
-scale_filename = 'violin_scalar_3files.pkl'
-scaler = pickle.load(open(scale_filename, "rb"))
+filename_list = Path(data_path).rglob("*.feats_ddcpca.npy")
 
-# shape: (n, 85)
-combined_feat = np.load(filename)
-scaled_feat = (combined_feat - scaler.mean_) / scaler.scale_
+# #sklearn StandardScaler (n_features=85)
+# scale_filename = 'violin_scalar_3files.pkl'
+# scaler = pickle.load(open(scale_filename, "rb"))
 
-#split into 2 files
-filename_base = filename.split(".")[0]
-    #mel80
-np.save(f"{filename_base}.audio_mel80.npy", scaled_feat[:,:80])
-    #beats5
-np.save(f"{filename_base}.audio_beats5.npy", scaled_feat[:,80:])
+scaler = StandardScaler()
+filename_array = []
+data = []
+for filename in filename_list:
+    filename_base = str(filename.stem).split(".")[0]  # str(filename.parent)
+    # shape: (n, 85)
+    combined_feat = np.load(filename)
+    filename_array.append((filename_base, combined_feat.shape[0]))
+    data.append(combined_feat)
+
+data = np.concatenate(data, axis=0)
+scaler.fit(data)
+pickle.dump(scaler, open("feat_scalar.pkl", "wb"))
+
+data = scaler.transform(data)
+# data = (data - scaler.mean_) / scaler.scale_
+
+for filename_base, length in filename_array:
+    print(filename_base)
+    scaled_feat = data[:length]
+    data = data[length:]
+    # mel80
+    np.save(f"{filename_base}.audio_mel80.npy", scaled_feat[:, :80])
+    # beats5
+    np.save(f"{filename_base}.audio_beats5.npy", scaled_feat[:, 80:])
